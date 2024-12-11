ee1035ac5e3e12b2486d64db09a5c3647ff06aaf
Gary Ng Kwong Sang <gng4820@gmail.com>
2024-12-11 14:30:15 -0700

    Day 3 Part 2 Solution

--- Day 3: Mull It Over/3.py
+++ Day 3: Mull It Over/3.py
@@ -1,12 +1,29 @@
 import re
 
 def main():
+    ## Part 1
+    # mulOutputs = []
+    # with open("3.txt", "r") as file:
+    #     for line in file:
+    #         matches = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', line)
+    #         for x, y in matches:
+    #             mulOutputs.append(int(x) * int(y))
+
+    ## Part 2
     mulOutputs = []
     with open("3.txt", "r") as file:
+        disabled = False
         for line in file:
-            matches = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', line)
-            for x, y in matches:
-                mulOutputs.append(int(x) * int(y))
+            matches = re.findall(r'mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)', line)
+            print(matches)
+            for match in matches:
+                if match == "do()":
+                    disabled = False
+                elif match == "don't()":
+                    disabled = True
+                elif not disabled:
+                    x, y = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', match)[0]
+                    mulOutputs.append(int(x) * int(y))
 
     print(sum(mulOutputs))
 
